3d1ca030978774d0df9ff9454a8803390e73cde7
pavol <palodudak@gmail.com>
2020-02-01 15:24:38 +0100

    right before bulma took over

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,13 +2,25 @@ import React from 'react'
 import BlogLayout from '../components/layouts/blogLayout.js'
 
 const Item = () => (
-  <>
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-    <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
-    </p>
-  </>
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+        <p>
+          What do I like to do? Lots of course but definitely enjoy building
+          websites.
+        </p>
+      </div>
+    </div>
+  </div>
 )
 
-export default () => <BlogLayout>{Array(10).fill(1).map(()=><Item></Item>)}</BlogLayout>
+export default () => (
+  <BlogLayout>
+    {Array(10)
+      .fill(1)
+      .map(() => (
+        <Item></Item>
+      ))}
+  </BlogLayout>
+)
